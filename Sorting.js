2250aada15f24d06541a2fae90f233f5273d993e
Ganesh Patil <ganeshpatil67284@gmail.com>
2023-05-08 19:33:48 +0530

    initial commit

---
+++ Sorting.js
@@ -0,0 +1,17 @@
+
+let data=[34,56,78,90,21];
+
+for(let i=0; i<data.length; i++){
+    for(let j=0; j<data.length; j++){
+         
+        if(data[j]>data[j+1]){
+
+            
+            let temp=data[j];
+            data[j]=data[j+1];
+            data[j+1]=temp;
+        }
+    }
+}
+// console.log(data.sort());
+console.log(data)
